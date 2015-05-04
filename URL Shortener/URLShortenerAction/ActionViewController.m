c6736f4a05c20b9734d96f1fe82fcabe699f2dbd
Neeraj Baid <wdaareg@gmail.com>
2015-05-04 11:29:22 -0700

    Shows UI momentarily after shortening URL.

--- URL Shortener/URLShortenerAction/ActionViewController.m
+++ URL Shortener/URLShortenerAction/ActionViewController.m
@@ -1,14 +1,24 @@
 #import <MobileCoreServices/MobileCoreServices.h>
+#import <URLShortenerKit/URLShortenerKit.h>
 
 #import "ActionViewController.h"
 
-@interface ActionViewController ()
+@interface ActionViewController () <USKShortenerDelegate>
 
-@property(strong,nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (strong, nonatomic) USKShortener *shortener;
 
 @end
 
 @implementation ActionViewController
+
+- (USKShortener *)shortener {
+    if (!_shortener) {
+        _shortener = [[USKShortener alloc] initWithDelegate:self];
+    }
+    return _shortener;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -19,7 +29,10 @@
                 [itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeURL
                                                 options:nil
                                       completionHandler:^(NSURL *url, NSError *error) {
-                                          
+                                          dispatch_async(dispatch_get_main_queue(), ^{
+                                              [self.spinner startAnimating];
+                                          });
+                                          [self.shortener shortenURL:[url absoluteString]];
                                       }];
                 
                 found = YES;
@@ -32,8 +45,33 @@
     }
 }
 
-- (IBAction)done {
-    [self.extensionContext completeRequestReturningItems:self.extensionContext.inputItems completionHandler:nil];
+- (void)shortener:(USKShortener *)shortener
+    didShortenURL:(NSString *)originalURL
+   toShortenedURL:(NSString *)shortenedURL {
+    [[UIPasteboard generalPasteboard] setString:shortenedURL];
+    [self done];
+}
+
+- (void)shortener:(USKShortener *)shortener failedToShortenURL:(NSString *)originalURL {
+    self.statusLabel.text = @"shorten failed";
+    [self done];
+}
+
+- (void)done {
+    [UIView animateWithDuration:0.2 animations:^{
+        self.spinner.alpha = 0;
+        self.statusLabel.alpha = 1;
+    } completion:^(BOOL finished) {
+        [self.spinner stopAnimating];
+        [self performSelector:@selector(complete)
+                   withObject:nil
+                   afterDelay:1];
+    }];
+}
+
+- (void)complete {
+    [self.extensionContext completeRequestReturningItems:self.extensionContext.inputItems
+                                       completionHandler:nil];
 }
 
 @end
